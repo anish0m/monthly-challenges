6ea984cda125c82b7f49cfff8c1d142b010a21c2
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-02-12 17:14:57 +0600

    Redirect to month based on index

--- challenges/views.py
+++ challenges/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 
 
@@ -18,6 +18,13 @@ monthly_challenges = {
 }
 
 # Create your views here.
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    redirect_month = months[month]
+
+    HttpResponseRedirect("/challenges/" + redirect_month)
 
 
 def monthly_challenge(request, month):
