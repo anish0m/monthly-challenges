beeb9081aca8efda0122c4c74db3c7c084e0a1fe
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-02-12 16:09:08 +0600

    Replace if-else with dictionary

--- challenges/views.py
+++ challenges/views.py
@@ -1,37 +1,28 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
+
+monthly_challenges = {
+    "january": "Eat no carbs for the entire month!",
+    "february": "Walk for at least 20 minutes everyday!",
+    "march": "Learn Django for at least an hour everyday!",
+    "april": "Read at least one book per week!",
+    "may": "Go hiking every weekend!",
+    "june": "Drink 2 litres of water everyday!",
+    "july": "Change all wirings in your house!",
+    "august": "Make a handmade merchandise everyday!",
+    "september": "Watch a new movie every weekend!",
+    "october": "Participate in contest programming!",
+    "november": "Get a facial at least twice a week!",
+    "december": "Take herbal tea every morning!"
+}
+
 # Create your views here.
 
 
 def monthly_challenge(request, month):
-    challenge_text = None
-
-    if month == 'january':
-        challenge_text = "Eat no carbs for the entire month!"
-    elif month == 'february':
-        challenge_text = "Walk for at least 20 minutes everyday!"
-    elif month == 'march':
-        challenge_text = "Learn Django for at least an hour everyday!"
-    elif month == 'april':
-        challenge_text = "Read at least one book per week!"
-    elif month == 'may':
-        challenge_text = "Go hiking every weekend!"
-    elif month == 'june':
-        challenge_text = "Drink 2 litres of water everyday!"
-    elif month == 'july':
-        challenge_text = "Change all wirings in your house!"
-    elif month == 'august':
-        challenge_text = "Make a handmade merchandise everyday!"
-    elif month == 'september':
-        challenge_text = "Watch a new movie every weekend!"
-    elif month == 'october':
-        challenge_text = "Participate in contest programming!"
-    elif month == 'november':
-        challenge_text = "Get a facial at least twice a week!"
-    elif month == 'december':
-        challenge_text = "Take herbal tea every morning!"
-    else:
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound("Unsupported month!")
-
-    return HttpResponse(challenge_text)
