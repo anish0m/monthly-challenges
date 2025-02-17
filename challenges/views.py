f6b9315888924589200a50634a0053915d84a743
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-02-17 11:47:04 +0600

    Raise 404 error

--- challenges/views.py
+++ challenges/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
@@ -49,4 +49,4 @@ def monthly_challenge(request, month):
             "month_name": month
         })
     except:
-        return HttpResponseNotFound("Unsupported month!")
+        raise Http404()
